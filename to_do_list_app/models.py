701b4fde996527f8cfbf5b1b58e9574ddbd144a0
Aarya Dubhashi <aarya.dubhashi@gmail.com>
2023-04-08 22:42:02 -0400

    changed created to deadline

--- to_do_list_app/models.py
+++ to_do_list_app/models.py
@@ -8,11 +8,11 @@ class Task (models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
     complete = models.BooleanField(default = False)
-    created = models.DateTimeField(editable = True)
+    deadline = models.DateTimeField(editable = True, db_column='created')
     
     def __str__(self):
         return self.title
     
     class Meta:
-        ordering = ['complete', 'created']
+        ordering = ['complete', 'deadline']
         
--- to_do_list_app/views.py
+++ to_do_list_app/views.py
@@ -37,7 +37,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title','description','complete','created']
+    fields = ['title','description','complete','deadline']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -46,7 +46,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['title','description','complete','created']
+    fields = ['title','description','complete','deadline']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
