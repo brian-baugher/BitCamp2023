960729dc506b08424e5e2e39c9cedc2024b6c7ce
Aarya Dubhashi <aarya.dubhashi@gmail.com>
2023-04-08 20:13:39 -0400

    fixed errorrs where tasks were repeated between users

--- to_do_list_app/views.py
+++ to_do_list_app/views.py
@@ -25,6 +25,11 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user = self.request.user)
+        return context
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
